31c3d6dbe7a6fb226ca711a7a8a522be591a37b9
Hiregoudru Malegouda <hiregoudrum143@gmail.com>
2025-01-02 21:04:25 +0530

    Patient.c

---
+++ Patient.c
@@ -0,0 +1,19 @@
+#include "Patient.h"
+#include <stdio.h>
+
+// Function definitions
+void addPatient() {
+    // Code to add a patient
+}
+
+void viewPatients() {
+    // Code to view patients
+}
+
+void updatePatient() {
+    // Code to update patient information
+}
+
+void deletePatient() {
+    // Code to delete a patient
+}
